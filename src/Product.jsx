9a459efdf8efe9f99cf8cc39502f128f0c722324
Rashid Shamsudinov <rash2x@gmail.com>
2022-11-30 18:16:26 +0530

    add catalog ui

---
+++ src/Product.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import PicOne from './assets/pic1.png';
+import {ButtonBase, Chip, styled, Typography} from '@mui/material';
+
+const Base = styled(ButtonBase)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 16px;
+  
+  max-width: 50%;
+  width: 190px;
+  height: 240px;
+  
+  
+  img {
+    border-radius: 50%;
+    margin-top: 0;
+  }
+  
+  &.Mui-disabled {
+    pointer-events: all;
+  }
+`;
+
+const PlusMinus = styled('div')`
+  display: flex;
+  align-items: center;
+  > * {
+    margin: 0 8px;
+  }
+`;
+
+const ChipButton = styled(Chip)`
+  background: #fff;
+  width: 40px;
+  height: 32px;
+`;
+
+const Title = styled(Typography)`
+  margin: 12px 0;
+`;
+
+
+const Product = () => {
+  const [added, setAdded] = useState(0);
+
+  const handleClick = () => {
+    setAdded(added + 1);
+  };
+  const handlePlus = () => {
+    setAdded(added + 1);
+  };
+  const handleMinus = () => {
+    console.log(added);
+    setAdded(added - 1);
+  };
+
+
+  return (
+    <Base component="div" onClick={added === 0 ? handleClick : () => {}} sx={{
+      background: added ? '#F0D9FF' : 'inherit'
+    }}>
+      <img src={PicOne} alt=""/>
+      <Title>Pumpkin Milk Soup</Title>
+      {added === 0 && <Chip color="secondary" label="1100 LKR  􀐱15"></Chip>}
+      {added > 0 && <PlusMinus>
+        <ChipButton label="􀅽" onClick={handleMinus}></ChipButton>
+        {added}
+        <ChipButton label="􀅼" onClick={handlePlus}></ChipButton>
+      </PlusMinus>}
+    </Base>
+  )
+}
+
+export default Product;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 
-const theme = createTheme({});
+const theme = createTheme({
+  typography: {
+    fontFamily: '"SF Pro", "Helvetica Nueu", Roboto'
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
