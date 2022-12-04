056c3724e512e65d74c48d8c8de1e66c677ea278
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-04 22:13:42 +0530

    let's check

--- functions/bot/firebase.js
+++ functions/bot/firebase.js
@@ -1,8 +1,9 @@
-const {initializeApp, credential} = require('firebase-admin/app');
+const firebase = require('firebase-admin');
+const {initializeApp} = require('firebase-admin/app');
 const {getFirestore} = require('firebase-admin/firestore');
 
 initializeApp({
-  credential: credential.cert({
+  credential: firebase.credential.cert({
     'type': 'service_account',
     'project_id': process.env.FIREBASE_PROJECT_ID,
     'private_key_id': process.env.FIREBASE_PRIVATE_KEY_ID,
@@ -14,6 +15,12 @@ initializeApp({
     'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
     'client_x509_cert_url': process.env.CLIENT_X509_CERT_URL
   }),
+  authDomain: "lanka-496b2.firebaseapp.com",
+  projectId: "lanka-496b2",
+  storageBucket: "lanka-496b2.appspot.com",
+  messagingSenderId: "452591030865",
+  appId: "1:452591030865:web:b39ead8034fb16f9711e70",
+  measurementId: "G-KB7LCN4M23"
 });
 
 const firestore = getFirestore();
