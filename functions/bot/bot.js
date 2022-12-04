54398b823278c2d34ac126d845b17dc7b2662172
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-04 23:03:52 +0530

    change hello message

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -3,7 +3,6 @@ const {firestore} = require('./firebase.js');
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start(async (ctx) => {
-  console.log(ctx.update.message)
   if(!ctx.startPayload) return ctx.reply(`No Payload, eee`);
 
   const user = ctx.update.message.from;
@@ -12,13 +11,24 @@ bot.start(async (ctx) => {
   const orderRef = await firestore.collection('basket').doc(orderId);
   const order = await orderRef.get();
 
-  if(order.exists) {
-    await orderRef.update({
-      telegramUser: user
-    })
-  }
+  if(!order.exists) return ctx.reply('No order found');
 
-  return ctx.reply(`Hello` + user.username);
+  await orderRef.update({
+    telegramUser: user
+  })
+
+  const productsMessage = order.products.reduce(async (acc, product) => {
+    return acc + `${product.count} ${product.title} \n `
+  }, '')
+
+  const sum = order.products.reduce((acc, product) => acc + parseInt(product.count) * parseInt(product.price), 0)
+
+  return ctx.reply(`
+    Здравствуйте, мы сформировали ваш заказ \n
+    ${productsMessage}
+    на общую сумму ${sum} \n
+    Подскажите куда привезти товар?
+  ` + user.username);
 });
 
 const handler = async (event) => {
