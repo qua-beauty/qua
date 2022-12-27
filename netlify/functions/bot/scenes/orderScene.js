3dc7fcdad79287484203c57a9d1b7f56adb07726
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 02:46:31 +0530

    can't really understand what's going on

--- netlify/functions/bot/scenes/orderScene.js
+++ netlify/functions/bot/scenes/orderScene.js
@@ -5,13 +5,15 @@ const {Message} = require('../messages.js');
 
 const orderScene = new Scenes.WizardScene('ORDER_SCENE',
   async (ctx) => {
-  console.log(ctx);
+    console.log(ctx);
     const {message_id, text, chat: {id}} = ctx.update.message;
     const orderId = text.match(masks.order)[0].replace('#', '');
 
     await ctx.telegram.deleteMessage(id, message_id);
 
-    const order = await getOrder(orderId);
+    console.log(orderId);
+    const order = await getOrder(orderId).catch(e => console.log(e));
+
     await ctx.reply(Message.orderCreated(order));
 
     return ctx.wizard.next();
