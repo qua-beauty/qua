e23b1b78b066083a490cf3957b743c7a10d3d894
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 03:45:52 +0530

    wizard works strange

--- netlify/functions/bot/scenes/orderScene.js
+++ netlify/functions/bot/scenes/orderScene.js
@@ -8,13 +8,14 @@ const orderScene = new Scenes.WizardScene('ORDER_SCENE',
     const {message_id, text, chat: {id}} = ctx.update.message;
     const orderId = text.match(masks.order)[0].replace('#', '');
 
-    return getOrder(orderId).then((order) => {
+    getOrder(orderId).then((order) => {
       ctx.telegram.deleteMessage(id, message_id);
       ctx.reply(Message.orderCreated(order), {
         parse_mode: 'MarkdownV2'
       });
-      return ctx.wizard.next();
     });
+
+    return ctx.wizard.next();
   },
   (ctx) => {
     ctx.reply('I got your location thanks');
