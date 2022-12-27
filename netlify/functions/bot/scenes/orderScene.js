dfbdcfe6c6c00241bc8999600853c2e64558dc8f
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 03:39:31 +0530

    we are close to fix

--- netlify/functions/bot/scenes/orderScene.js
+++ netlify/functions/bot/scenes/orderScene.js
@@ -4,18 +4,17 @@ const {masks} = require('../utils.js');
 const {Message} = require('../messages.js');
 
 const orderScene = new Scenes.WizardScene('ORDER_SCENE',
-  async (ctx) => {
+  (ctx) => {
     const {message_id, text, chat: {id}} = ctx.update.message;
     const orderId = text.match(masks.order)[0].replace('#', '');
 
-    await ctx.telegram.deleteMessage(id, message_id);
-
-    console.log(orderId);
-    const order = await getOrder(orderId).catch(e => console.log(e));
-
-    await ctx.reply(Message.orderCreated(order));
-
-    return ctx.wizard.next();
+    return getOrder(orderId).then((order) => {
+      ctx.telegram.deleteMessage(id, message_id);
+      ctx.reply(Message.orderCreated(order), {
+        parse_mode: 'MarkdownV2'
+      });
+      return ctx.wizard.next();
+    });
   },
   (ctx) => {
     ctx.reply('I got your location thanks');
