658068bd1e7073322171603135bfbe06513f3982
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 02:32:03 +0530

    let's try delete message

--- netlify/functions/bot/scenes/startScene.js
+++ netlify/functions/bot/scenes/startScene.js
@@ -4,7 +4,9 @@ const {Message} = require('../messages.js');
 const startScene = new Scenes.BaseScene('WELCOME_SCENE');
 
 startScene.enter((ctx) => {
+  const {message_id, chat: {id: chat_id}} = ctx.update.message;
   ctx.reply(Message.start, Keyboard.start);
+  ctx.telegram.deleteMessage(chat_id, message_id);
   ctx.scene.leave();
 });
 
