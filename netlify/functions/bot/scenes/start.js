3e1b84e8797369e3628416188e1775a88a8f0926
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 07:12:43 +0530

    import error fix 2

--- netlify/functions/bot/scenes/start.js
+++ netlify/functions/bot/scenes/start.js
@@ -3,17 +3,17 @@ const {keyboards} = require('../keyboards.js');
 const {messages} = require('../messages.js');
 const {sceneNames} = require('../constants.js');
 
-const start = new Scenes.BaseScene(sceneNames.START);
+const startScene = new Scenes.BaseScene(sceneNames.START);
 
-start.enter(async (ctx) => {
+startScene.enter(async (ctx) => {
   await ctx.reply(messages.start, keyboards.start);
   ctx.scene.leave();
 });
 
-start.leave((ctx) => {
+startScene.leave((ctx) => {
   console.log('User left start scene');
 });
 
 module.exports = {
-  start
+  startScene
 };
