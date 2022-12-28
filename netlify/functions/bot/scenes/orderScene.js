bf9dd308182466323416b70d8daf508d589ae3e6
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 05:50:45 +0530

    something wrong with wizardScene

--- netlify/functions/bot/scenes/orderScene.js
+++ netlify/functions/bot/scenes/orderScene.js
@@ -6,14 +6,20 @@ const {Message} = require('../messages.js');
 const orderScene = new Scenes.WizardScene('ORDER_SCENE',
   async (ctx) => {
     const {text} = ctx.update.message;
-    const orderId = text.match(masks.order)[0].replace('#', '');
-    const order = await getOrder(orderId);
+    const match = text.match(masks.order);
 
-    await ctx.reply(Message.orderCreated(order), {
-      parse_mode: 'MarkdownV2'
-    });
+    if (match) {
+      const orderId = match[0].replace('#', '');
+      const order = await getOrder(orderId);
 
-    return await ctx.wizard.next();
+      await ctx.reply(Message.orderCreated(order), {
+        parse_mode: 'MarkdownV2'
+      });
+    } else {
+      return ctx.scene.leave();
+    }
+
+    return ctx.wizard.next();
   },
   async (ctx) => {
     await ctx.reply('I got your location thanks');
