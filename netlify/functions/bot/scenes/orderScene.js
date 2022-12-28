d13f32e683bc2f6b94e3879d08656bbfa579082c
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-28 05:36:26 +0530

    hm

--- netlify/functions/bot/scenes/orderScene.js
+++ netlify/functions/bot/scenes/orderScene.js
@@ -5,11 +5,11 @@ const {Message} = require('../messages.js');
 
 const orderScene = new Scenes.WizardScene('ORDER_SCENE',
   async (ctx) => {
-    const {message_id, text, chat: {id}} = ctx.update.message;
     const orderId = text.match(masks.order)[0].replace('#', '');
     const order = await getOrder(orderId);
 
-    await ctx.telegram.deleteMessage(id, message_id);
+    console.log(order)
+
     await ctx.reply(Message.orderCreated(order), {
       parse_mode: 'MarkdownV2'
     });
