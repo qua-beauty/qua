a71f92ed396203064d0498cea5bb472574bb153b
Rashid Shamsudinov <rash2x@gmail.com>
2022-12-29 18:11:44 +0530

    update message

--- src/telegram.js
+++ src/telegram.js
@@ -11,12 +11,12 @@ const TWAMessages = {
     const {products, comment, status, id} = data;
 
     const productsMessage = products.reduce((acc, product, index) => {
-      return acc + `\\- ${product.icon} ${product.title} \\(${product.count} x ${product.price}\\)\n`;
+      return acc + `${product.icon} ${product.title} \\(${product.count} x ${product.price}\\)\n`;
     }, '');
 
     const sum = products.reduce((acc, product) => acc + parseInt(product.count) * parseInt(product.price), 0);
 
-    return `\\#${id}\n\nМой заказ:\\!\n${productsMessage}\n*Общая сумма: ${sum}*\n*Комментарий:* ${comment}\n*Статус*: ${getStatusTitle(status)}`;
+    return `\\#${id}\n\nМой заказ:\n${productsMessage}\n*Общая сумма: ${sum}*\n*Комментарий:* ${comment}\n*Статус*: ${getStatusTitle(status)}`;
   }
 }
 
