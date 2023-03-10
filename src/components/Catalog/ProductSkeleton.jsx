44d68d78f65c234f16f60d3be6c4351ac21ec3ef
Rashid Shamsudinov <rash2x@gmail.com>
2023-03-10 11:50:03 +0530

    update skeleton color

--- src/components/Catalog/ProductSkeleton.jsx
+++ src/components/Catalog/ProductSkeleton.jsx
@@ -4,9 +4,9 @@ import {Skeleton, Stack} from '@chakra-ui/react';
 const ProductSkeleton = () => {
   return (
     <Stack alignItems={'center'}>
-      <Skeleton width={'292px'} borderRadius={'32px'} height='220px' />
-      <Skeleton width={'272px'} borderRadius={'12px'} height='20px' />
-      <Skeleton width={'272px'} borderRadius={'12px'} height='20px' />
+      <Skeleton startColor='background.default' endColor='telegram.300' width={'292px'} borderRadius={'32px'} height='220px' />
+      <Skeleton startColor='background.default' endColor='telegram.300' width={'272px'} borderRadius={'12px'} height='20px' />
+      <Skeleton startColor='background.default' endColor='telegram.300' width={'272px'} borderRadius={'12px'} height='20px' />
     </Stack>
   );
 };
