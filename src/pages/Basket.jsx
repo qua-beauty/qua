ec77a946e21d383a1d897a3f351bb9f0c1f82de7
rashdeva <rasshdeva@gmail.com>
2023-03-16 00:41:39 +0300

    try to useef

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -58,18 +58,20 @@ const Basket = () => {
     }
   }, [basket, currentShop])
 
-  if (webApp) {
-    webApp.MainButton.text = `${t('basket.continueButton')} ${count > 0 && `(${count}x${price} ${t(`currency.${currency}`, { ns: 'common' })})`}`;
-    webApp.MainButton.color = '#66bb6a';
-    webApp.MainButton.enable();
-    webApp.MainButton.onClick(handleMakeOrder);
-    webApp.BackButton.show();
-    webApp.BackButton.onClick(() => {
-      navigate('/');
-    });
-  }
+
 
   useEffect(() => {
+    if (webApp) {
+      webApp.MainButton.text = `${t('basket.continueButton')} ${count > 0 && `(${count}x${price} ${t(`currency.${currency}`, { ns: 'common' })})`}`;
+      webApp.MainButton.color = '#66bb6a';
+      webApp.MainButton.enable();
+      webApp.MainButton.onClick(handleMakeOrder);
+      webApp.BackButton.show();
+      webApp.BackButton.onClick(() => {
+        navigate('/');
+      });
+    }
+
     return () => {
       if (webApp) {
         webApp.MainButton.offClick(handleMakeOrder);
