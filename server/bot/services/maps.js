9ed4a5d58bffdf96a9084dc6a544973e29c750e5
Rashid Shamsudinov <rash2x@gmail.com>
2023-04-18 00:03:55 +0530

    enough to use earth, thanks chatgpt

--- server/bot/services/maps.js
+++ server/bot/services/maps.js
@@ -1,19 +1,19 @@
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
 export const getDistance = async (shopCoords, placeCoords) => {
-  const url = `https://api.mapbox.com/directions-matrix/v1/mapbox/driving/${shopCoords.longitude},${shopCoords.latitude};${placeCoords.longitude},${placeCoords.latitude}?access_token=${Deno.env.get('MAPBOX_TOKEN')}`;
+    const R = 6371; // radius of the earth in kilometers
+    const dLat = deg2rad(placeCoords.latitude - shopCoords.latitude);
+    const dLon = deg2rad(placeCoords.longitude - shopCoords.longitude);
+    const lat1Rad = deg2rad(shopCoords.latitude);
+    const lat2Rad = deg2rad(placeCoords.latitude);
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
 
-    if (data.code !== "Ok") {
-      console.log(`Failed to retrieve distance: ${data.status}`);
-      return false;
-    }
-
-    data.destinations.sort((a, b) => parseFloat(a.distance) - parseFloat(b.distance));
-    return data.destinations[0].distance;
-  } catch (e) {
-    console.log(e);
-    return false;
-  }
+    return parseFloat(distance.toFixed(2));
 };
