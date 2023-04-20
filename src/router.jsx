8e6d80cf514906c3f313d26f3bc62c3eb318be9f
Rashid Shamsudinov <rash2x@gmail.com>
2023-04-20 21:20:20 +0530

    add shops select page

--- src/router.jsx
+++ src/router.jsx
@@ -12,11 +12,11 @@ const router = createBrowserRouter([
     element: (<App/>),
     children: [
       {
-        path: 'shops',
+        path: '',
         element: (<Shops/>),
       },
       {
-        path: '',
+        path: 'shop/:shopId',
         element: (<Catalog/>),
       },
       {
