3181f39f621ace1c097a38cf6dfbfea1f346a232
Vova <vpr@ggr.group>
2023-05-23 13:30:00 +0300

    change to be styled as a column

--- src/components/Shops/ShopItem.jsx
+++ src/components/Shops/ShopItem.jsx
@@ -14,34 +14,19 @@ const ShopItem = ({onSelect, ...shop}) => {
       <Flex sx={{
         flexDirection: 'column',
       }} onClick={() => onSelect(shop)}>
-        <Box sx={{
-          background: `var(--chakra-colors-background-default) url(${thumbnail}) no-repeat center`,
-          filter: isWorking ? 'none' : 'grayscale(1)',
-          backgroundSize: 'cover',
-          ...borderRadius(16),
+        <Flex direction={"column"} sx={{
           width: '100%',
           height: 200,
-          display: 'flex',
           alignItems: 'center',
           position: 'relative',
           overflow: 'hidden',
+          justifyContent: 'center',
+          gap: '8px',
         }}>
-          {logo && <Box width={'48px'} height={'48px'} borderRadius={'50%'} position={'absolute'} bottom={'8px'} left={'8px'} overflow={'hidden'}>
+          {logo && <Box width={'120px'} height={'120px'} borderRadius={'50%'} overflow={'hidden'}>
             <img src={logo} width={'100%'} alt=""/>
           </Box>}
-        </Box>
-        <Flex p={'0 8px'} mt={'8px'} justifyContent={'space-between'} alignItems={'center'}>
           <Heading fontSize={'xl'}>{name}</Heading>
-          <Flex alignItems={'center'} gap={'6px'}>
-            <Text fontWeight={'500'} fontSize={'md'}>
-              {isWorking ? <>
-                <Online/> {t('info.status.active')}
-              </> : <>
-                <Offline/> {available ? t('info.status.inactive') : ''}
-              </>}
-              {isWorking ? shop.workTime : (available ? shop.startTime : t('info.status.closed'))}
-            </Text>
-          </Flex>
         </Flex>
       </Flex>
   );
