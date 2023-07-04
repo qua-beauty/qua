703523803f955e532cb74340bc7c96c919c79c66
Rashid Shamsudinov <rash2x@gmail.com>
2023-07-04 19:01:13 +0530

    add transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import useAuth from './hooks/useAuth.js';
-import {Outlet, useParams} from 'react-router-dom';
-import {useGetCategoriesQuery, useGetProductsQuery, useGetShopsQuery} from './api/api.js';
-import {useDispatch, useSelector} from 'react-redux';
-import {setCurrentProduct, setProductsData} from './api/slices/productSlice.js';
-import {setCategoriesData} from './api/slices/categorySlice.js';
-import {setCurrentShop, setShopsData} from './api/slices/shopSlice.js';
+import { Outlet, useParams } from 'react-router-dom';
+import { useGetCategoriesQuery, useGetProductsQuery, useGetShopsQuery } from './api/api.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentProduct, setProductsData } from './api/slices/productSlice.js';
+import { setCategoriesData } from './api/slices/categorySlice.js';
+import { setCurrentShop, setShopsData } from './api/slices/shopSlice.js';
 import './i18n';
-import {useTranslation} from 'react-i18next';
-import {Box} from '@chakra-ui/react';
-import {withProfiler} from '@sentry/react';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@chakra-ui/react';
+import { withProfiler } from '@sentry/react';
+import {
+  TransitionGroup,
+  CSSTransition
+} from "react-transition-group";
 
 function App() {
   useAuth();
   const dispatch = useDispatch();
-  const {productId, shopId} = useParams();
-  const {i18n} = useTranslation();
+  const { productId, shopId } = useParams();
+  const { i18n } = useTranslation();
 
   const user = useSelector(state => state.user.data);
-  const {data: shops, isLoading: isShopsLoading} = useGetShopsQuery();
-  const {data: products, isLoading: isProductsLoading} = useGetProductsQuery();
-  const {data: categories, isLoading: isCategoriesLoading} = useGetCategoriesQuery();
+  const { data: shops, isLoading: isShopsLoading } = useGetShopsQuery();
+  const { data: products, isLoading: isProductsLoading } = useGetProductsQuery();
+  const { data: categories, isLoading: isCategoriesLoading } = useGetCategoriesQuery();
 
   useEffect(() => {
     if (user && user.language) {
@@ -60,8 +64,22 @@ function App() {
 
   return (
     <Box maxW={480} m={'0 auto'}>
-      <Outlet isLoading={isLoading}/>
-    </Box>
+      <TransitionGroup>
+        {/*
+            This is no different than other usage of
+            <CSSTransition>, just make sure to pass
+            `location` to `Switch` so it can match
+            the old location as it animates out.
+          */}
+        <CSSTransition
+          key={location.pathname}
+          classNames="fade"
+          timeout={300}
+        >
+        <Outlet isLoading={isLoading} />
+      </CSSTransition>
+    </TransitionGroup>
+    </Box >
   );
 }
 
--- src/components/Shop/Product.jsx
+++ src/components/Shop/Product.jsx
@@ -2,36 +2,44 @@ import React, { FunctionComponent, useMemo } from "react";
 
 const Product = ({
   onSelect,
-  height,
-  cursor,
-  justifyContent,
-  design,
-  duration,
-  price,
+  frame44Height,
+  frame44Cursor,
+  frame65JustifyContent,
+  simpleDesign,
+  m,
+  prop,
 }) => {
-  const frameDivStyle = useMemo(() => ({
-    height,
-    cursor,
-  }), [height, cursor]);
+  const frameDivStyle = useMemo(() => {
+    return {
+      height: frame44Height,
+      cursor: frame44Cursor,
+    };
+  }, [frame44Height, frame44Cursor]);
 
-  const frameDiv1Style = useMemo(() => ({
-    justifyContent,
-  }), [justifyContent]);
+  const frameDiv1Style = useMemo(() => {
+    return {
+      justifyContent: frame65JustifyContent,
+    };
+  }, [frame65JustifyContent]);
 
   return (
-    <div className="self-stretch rounded-2xl h-[3.38rem] flex flex-row items-center justify-between text-left text-[1.13rem] text-text-primary font-sf-pro-display"
+    <div
+      className="self-stretch rounded-2xl h-[3.38rem] flex flex-row items-center justify-between text-left text-[1.13rem] text-text-primary font-sf-pro-display"
       style={frameDivStyle}
       onClick={onSelect}
     >
-      <div className="flex flex-col items-start justify-start gap-[0.25rem]" style={frameDiv1Style}>
-        <div className="relative tracking-[-0.02em]">{design}</div>
+      <div
+        className="flex flex-col items-start justify-start gap-[0.25rem]"
+        style={frameDiv1Style}
+      >
+        <div className="relative tracking-[-0.02em]">{simpleDesign}</div>
         <div className="relative text-[0.94rem] tracking-[-0.03em] font-medium font-sf-pro-text text-text-secondary">
-          {duration} min
+          {m} min
         </div>
       </div>
       <div className="flex flex-row items-center justify-end text-right text-[1rem] text-text-white font-sf-pro-text">
-        <div className="rounded-md bg-telegram backdrop-filter blur-[8px] h-[2rem] overflow-hidden flex flex-row py-[0.13rem] px-[0.5rem] box-border items-center justify-center">
-          <div className="relative tracking-[-0.03em] font-medium">${price}</div>
+        <div className="rounded-md bg-telegram [backdrop-filter:blur(8px)] h-[2rem] overflow-hidden flex flex-row py-[0.13rem] px-[0.5rem] box-border items-center justify-center">
+          <div className="relative tracking-[-0.03em] font-medium">${prop}</div>
         </div>
       </div>
     </div>
